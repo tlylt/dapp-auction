f8c05f241b329b1f1048d3a02288e8df0247caba
bloppers <wencheng1298@gmail.com>
2022-10-14 11:07:12 +0800

    complete minting of nft feature

--- truffle/contracts/MintNFT.sol
+++ truffle/contracts/MintNFT.sol
@@ -1,27 +1,78 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity >=0.7.0 <0.9.0;
-// this contract is used to deploy and acuqire NFT to test the auction functionality
-//https://docs.openzeppelin.com/contracts/2.x/api/token/erc721
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MintNFT is ERC721, Ownable {
-    uint256 public mintPrice = 0.001 ether; // price of nft
-    uint256 public totalSupply;
-    bool public isMintEnabled;
+contract MintNFT is Ownable, ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    using Strings for uint256;
+    // Optional mapping for token URIs
+    mapping(uint256 => string) private _tokenURIs;
 
-    constructor() payable ERC721("Mint NFT", "MINTNFT") {}
+    // Base URI
+    string private _baseURIextended;
 
-    function toggleIsMintEnabled() external onlyOwner {
-        // Owner must enable the mint
-        isMintEnabled = !isMintEnabled;
+    constructor() public ERC721("MintNFT", "MintNFT") {}
+
+    // https://forum.openzeppelin.com/t/function-settokenuri-in-erc721-is-gone-with-pragma-0-8-0/5978/3
+    function setBaseURI(string memory baseURI_) external onlyOwner {
+        _baseURIextended = baseURI_;
     }
 
-    function mint() external payable {
-        require(isMintEnabled, "Minting not enabled");
-        require(msg.value == mintPrice, "wrong value");
-        totalSupply++;
-        uint256 tokenId = totalSupply;
-        _safeMint(msg.sender, tokenId); // see doc on line 4
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI)
+        internal
+        virtual
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI set of nonexistent token"
+        );
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseURIextended;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+
+        // If there is no base URI, return the token URI.
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+        // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+        // If there is a baseURI but no tokenURI, concatenate the tokenID to the baseURI.
+        return string(abi.encodePacked(base, tokenId.toString()));
+    }
+
+    function mint(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
     }
 }
