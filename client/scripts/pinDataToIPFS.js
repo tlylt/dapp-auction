57ac4250127a5b97a74c24e987738e5ea9e8a819
bloppers <wencheng1298@gmail.com>
2022-10-15 14:47:48 +0800

    add metadata to pinned files

--- client/scripts/pinDataToIPFS.js
+++ client/scripts/pinDataToIPFS.js
@@ -2,16 +2,21 @@ require("dotenv").config();
 const fs = require("fs");
 const axios = require("axios");
 const FormData = require("form-data");
-const { storeDataToFile } = require("./ipfsHelper.js");
+const { storeDataToFile, fillIpfsHashToMetaData } = require("./ipfsHelper.js");
+const pinMetaData = require("./pinMetaData");
 
 // Calls Pinata API's to pin file to IPFS
-const pinFileToIPFS = async (filePath) => {
+const pinDataToIPFS = async (filePath) => {
+    // filePath = metadata filepath
     const pinataEndpoint = process.env.PINATA_ENDPOINT;
     const pinataApiKey = process.env.PINATA_API_KEY;
     const pinataApiSecret = process.env.PINATA_API_SECRET;
     const form_data = new FormData();
+
+    await fillIpfsHashToMetaData(filePath);
     try {
         form_data.append("file", fs.createReadStream(filePath));
+        // console.log(form_data);
         const request = {
             method: "post",
             url: pinataEndpoint,
@@ -23,14 +28,15 @@ const pinFileToIPFS = async (filePath) => {
             },
             data: form_data,
         };
-        console.log("request:", request);
+        // console.log("request:", request);
         const response = await axios(request);
-        console.log("Successfully pinned file to IPFS : ", response);
+        // console.log("Successfully pinned file to IPFS : ", response);
         await storeDataToFile(response.data);
-        console.log("Successfully added IPFS response to json file");
+        console.log("Successfully pinned data, added response to json file");
+        await pinMetaData(filePath);
     } catch (err) {
         console.log("Error occurred while pinning file to IPFS: ", err);
     }
 };
 
-module.exports = pinFileToIPFS;
+module.exports = pinDataToIPFS;
