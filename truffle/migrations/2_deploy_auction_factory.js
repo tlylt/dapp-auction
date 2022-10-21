e7c8c6024e26016b5fea4eb316626a041df66de1
bloppers <wencheng1298@gmail.com>
2022-10-21 14:19:05 +0800

    Implement auction factory
    * Creation of auction factory
    * fix: syntax error in auction
    
    Co-authored-by: tlylt <liu_yongliang@hotmail.com>

---
+++ truffle/migrations/2_deploy_auction_factory.js
@@ -0,0 +1,11 @@
+// const Auction = artifacts.require("Auction");
+
+// module.exports = function (deployer) {
+//   deployer.deploy(Auction);
+// };
+
+const AuctionFactory = artifacts.require("AuctionFactory");
+
+module.exports = function (deployer) {
+    deployer.deploy(AuctionFactory);
+};
