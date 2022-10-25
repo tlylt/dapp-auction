d18c44cdcb95c5c577970667dd355c52f6522b52
bloppers <wencheng1298@gmail.com>
2022-10-25 16:02:12 +0800

    Feat: Create new auction (#7)
    create auction button and call abi method

--- truffle/contracts/AuctionFactory.sol
+++ truffle/contracts/AuctionFactory.sol
@@ -8,19 +8,26 @@ contract AuctionFactory {
     event ContractCreated(address newContractAddress);
 
     function createNewAuction(
-        IERC721 _nft,
-        uint _nftId,
-        uint _startingBid,
-        uint _increment
+        address _nft,
+        uint256 _nftId,
+        uint256 _startingBid,
+        uint256 _increment
     ) public {
         Auction auction = new Auction(
             msg.sender,
-            _nft,
+            IERC721(_nft),
             _nftId,
             _startingBid,
             _increment
         );
         auctions.push(auction);
         emit ContractCreated(address(auction));
+    }
+
+    function getAuctions() external view returns (Auction[] memory _auctions) {
+        _auctions = new Auction[](auctions.length);
+        for (uint i = 0; i < auctions.length; i++) {
+            _auctions[i] = auctions[i];
+        }
     }
 }
