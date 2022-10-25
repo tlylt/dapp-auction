5dd96b3d0fc1db9a0d91eac2abea9b42eb2a14b0
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-10-25 15:59:55 +0800

    Update Auction Contract (#6)
    1. Adjust compiler version for handling overflow
    2. Update README
    3. Format code and edit the require statement for the func 'bid' to handle the case of "starting e.g. 10eth, increment = 20eth", so that user can start the bid with 10 eth or less than increment

--- truffle/contracts/Auction.sol
+++ truffle/contracts/Auction.sol
@@ -1,47 +1,47 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 interface IERC721 {
-    function transfer(address, uint) external;
+    function transfer(address, uint256) external;
 
     function transferFrom(
         address,
         address,
-        uint
+        uint256
     ) external;
 }
 
 contract Auction is ReentrancyGuard {
     event Start();
-    event End(address highestBidder, uint highestBid);
-    event Bid(address indexed sender, uint amount);
-    event Withdraw(address indexed bidder, uint amount);
+    event End(address highestBidder, uint256 highestBid);
+    event Bid(address indexed sender, uint256 amount);
+    event Withdraw(address indexed bidder, uint256 amount);
 
     address payable public immutable seller;
 
     bool public started;
     bool public ended;
 
-    uint public startAt;
-    uint public duration;
-    uint public endAt;
-    uint public increment;
+    uint256 public startAt;
+    uint256 public duration;
+    uint256 public endAt;
+    uint256 public increment;
 
     IERC721 public nft; // address of the NFT
-    uint public nftId;
+    uint256 public nftId;
 
-    uint public highestBid;
+    uint256 public highestBid;
     address public highestBidder;
-    mapping(address => uint) public bids;
+    mapping(address => uint256) public bids;
 
     constructor(
         address sender,
         IERC721 _nft,
-        uint _nftId,
-        uint _startingBid,
-        uint _increment
+        uint256 _nftId,
+        uint256 _startingBid,
+        uint256 _increment
     ) {
         require(_startingBid > 0, "Starting bid must be greater than 0!");
         require(_increment > 0, "Increment must be greater than 0!");
@@ -52,7 +52,7 @@ contract Auction is ReentrancyGuard {
         nftId = _nftId;
     }
 
-    function start(uint _duration, uint _startTime) external {
+    function start(uint256 _duration, uint256 _startTime) external {
         require(!started, "Auction already started!");
         require(msg.sender == seller, "You are not the owner of this auction!");
         require(
@@ -70,7 +70,7 @@ contract Auction is ReentrancyGuard {
         emit Start();
     }
 
-    function getPrice() public view returns (uint) {
+    function getPrice() public view returns (uint256) {
         return highestBid;
     }
 
@@ -80,13 +80,13 @@ contract Auction is ReentrancyGuard {
         returns (
             address,
             address,
-            uint,
-            uint,
-            uint,
-            uint,
-            uint,
-            uint,
-            uint,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
             bool,
             bool
         )
@@ -110,7 +110,14 @@ contract Auction is ReentrancyGuard {
         require(started, "Auction not started!");
         require(!ended, "Auction ended!");
         require(block.timestamp < endAt, "Auction ended!");
-        require(msg.value >= increment, "Insufficient bid amount increment!");
+        if (highestBidder != address(0)) {
+            // only check if this is NOT the first offer
+            // as the msg.value in first offer represents the starting bid
+            require(
+                msg.value >= increment,
+                "Insufficient bid amount increment!"
+            );
+        }
         require(
             msg.value + bids[msg.sender] > highestBid,
             "Bid must be greater than highest bid!"
@@ -130,7 +137,7 @@ contract Auction is ReentrancyGuard {
     function withdraw() external payable nonReentrant {
         require(started, "Auction not started!");
         require(msg.sender != highestBidder, "Highest bidder cannot withdraw.");
-        uint bal = bids[msg.sender];
+        uint256 bal = bids[msg.sender];
         require(bal > 0, "No balance to withdraw.");
 
         bids[msg.sender] = 0; // Ensure all state changes happen before calling external contracts
