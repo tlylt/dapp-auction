121594b7a70b767734284a43f0f7477acb17e41c
Liu YongLiang <41845017+tlylt@users.noreply.github.com>
2022-10-29 11:43:38 +0800

    Setup landing page (#8)
    * Setup landing page
    * Add credit

---
+++ client/src/Landing.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from './components/Header';
+import Explainer from './components/Explainer';
+import { Helmet } from 'react-helmet-async';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(assets/nft4.webp)`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    overflowX: 'hidden',
+  },
+  mid: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: '2rem',
+  },
+}));
+export default function Landing() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Helmet>
+        <title>NFT Auction</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="NFT Auction" />
+      </Helmet>
+      <CssBaseline />
+      <Header />
+      <div className={classes.mid}>
+        <h1>Sell & Bid on NFTs Auctions Governed by Smart Contract</h1>
+      </div>
+      <Explainer />
+    </div>
+  );
+}
---
+++ client/src/components/Explainer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ImageCard from './ImageCard';
+import explanations from '../static/Explanation';
+import useWindowPosition from '../hook/useWindowPosition';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    [theme.breakpoints.down('md')]: {
+      flexDirection: 'column',
+    },
+  },
+}));
+export default function Explainer() {
+  const classes = useStyles();
+  const checked = useWindowPosition('header');
+  return (
+    <div className={classes.root} id="explainers">
+      {explanations.map((explanation) => (
+        <ImageCard item={explanation} checked={checked} />
+      ))}
+    </div>
+  );
+}
