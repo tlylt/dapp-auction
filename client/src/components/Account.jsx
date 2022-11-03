3497ee60871d0ede8ae5aa95b67d63a4d574c1ce
tlylt <liu_yongliang@hotmail.com>
2022-11-03 12:34:07 +0800

    Improve UI

--- client/src/components/Account.jsx
+++ client/src/components/Account.jsx
@@ -1,7 +1,26 @@
-import { Button, Card, Box, Grid, Typography, Divider } from '@mui/material';
+import { Card, Box, Grid, Typography, Divider } from '@mui/material';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useEth } from '../contexts/EthContext';
+import { displayInGwei, displayInHours } from '../utils';
 import NftApprovalCard from './NftApprovalCard';
 
-function Account() {
+function Account({ auctions }) {
+  const [auction, setAuction] = useState();
+  const {
+    state: { accounts },
+  } = useEth();
+
+  useEffect(() => {
+    if (auctions !== undefined && auctions.length > 0) {
+      for (let i = 0; i < auctions.length; i++) {
+        if (auctions[i].seller === accounts[0]) {
+          setAuction(auctions[i]);
+        }
+      }
+    }
+  }, [auctions, accounts]);
+
   return (
     <Card>
       <Grid spacing={0} container>
@@ -15,45 +34,54 @@ function Account() {
             >
               Your Latest Auction
             </Typography>
-            <Box>
-              <Typography variant="h1" gutterBottom>
-                xxxxxx
-              </Typography>
-              <Typography
-                variant="h4"
-                fontWeight="normal"
-                color="text.secondary"
-              >
-                Current Highest Bid Amount
-              </Typography>
-              <Box
-                display="flex"
-                sx={{
-                  py: 4,
-                }}
-                alignItems="center"
-              >
-                <Box>
-                  <Typography variant="h3">Auction Details</Typography>
-                  <Typography variant="h4">Start time:</Typography>
-                  <Typography variant="h4">Minimal increment:</Typography>
-                  <Typography variant="h4">Duration:</Typography>
-                  <Typography variant="h4">End time:</Typography>
+            {auction ? (
+              <Box>
+                <Typography variant="h1" gutterBottom>
+                  {displayInGwei(auction.highestBid)} gwei
+                </Typography>
+                <Typography
+                  variant="h4"
+                  fontWeight="normal"
+                  color="text.secondary"
+                >
+                  Current Highest Bid Amount
+                </Typography>
+                <Box
+                  display="flex"
+                  sx={{
+                    py: 4,
+                  }}
+                  alignItems="center"
+                >
+                  <Box>
+                    <Typography variant="h3">
+                      <a href={`#${auction.auctionContract._address}`}>
+                        Go to Auction
+                      </a>
+                    </Typography>
+                    <Typography variant="h4">
+                      Title:{auction.pinataMetadata.name}
+                    </Typography>
+                    <Typography variant="h4">
+                      Minimal increment:{displayInGwei(auction.increment)} gwei
+                    </Typography>
+                    <Typography variant="h4">
+                      Duration:{displayInHours(auction.duration)} hours
+                    </Typography>
+                    <Typography variant="h4">
+                      Start time:{auction.startAt}
+                    </Typography>
+                    <Typography variant="h4">
+                      End time:{auction.endAt}
+                    </Typography>
+                  </Box>
                 </Box>
               </Box>
-            </Box>
-            <Grid container spacing={3}>
-              <Grid sm item>
-                <Button fullWidth variant="outlined">
-                  Start Auction
-                </Button>
-              </Grid>
-              <Grid sm item>
-                <Button fullWidth variant="contained">
-                  End Auction
-                </Button>
-              </Grid>
-            </Grid>
+            ) : (
+              <Typography variant="h3">
+                You have not created any auctions yet...
+              </Typography>
+            )}
           </Box>
         </Grid>
         <Grid
